e63d7ea4460f80bbe5dd898690877218814823b8
Dave Kordalewski <davekordalewski@gmail.com>
2026-05-30 03:03:31 +0800

    Add twl dictionary

--- src/dict/scrabbledicts.ts
+++ src/dict/scrabbledicts.ts
@@ -1,4 +1,6 @@
 import { BiDirectionalPrefixDictionary } from "./dict";
 import { sowpodsWords } from "./sowpods";
+import { twl06 } from "./twl06";
 
 export const SowpodsDictionary = new BiDirectionalPrefixDictionary(sowpodsWords);
+export const Twl06Dictionary = new BiDirectionalPrefixDictionary(twl06);
